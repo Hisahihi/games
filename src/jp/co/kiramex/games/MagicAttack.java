8e5d24f2eac19ba159ca9028032bd846ea88c031
Hisahihi <jiuqingshui47@gmail.com>
2024-02-10 11:00:26 +0900

    現在の状態

---
+++ src/jp/co/kiramex/games/MagicAttack.java
@@ -0,0 +1,6 @@
+package jp.co.kiramex.games;
+
+public interface MagicAttack {
+	void magic_attack(Character opponent);
+
+}
---
+++ src/jp/co/kiramex/polymorphism/Dachshund.java
@@ -0,0 +1,12 @@
+package jp.co.kiramex.polymorphism;
+
+public class Dachshund extends Dog{
+
+	@Override
+	public void barking() {
+		System.out.println("ダックスフントは、キャンキャン！と鳴く");
+	}
+
+
+
+}
